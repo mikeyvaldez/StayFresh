71749da716a8697137c5a1e83cb31059f75245c6
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-02-14 13:13:11 -0800

    Terrarium complete

---
+++ Terrerium/script.js
@@ -0,0 +1,49 @@
+function drageElement(terrariumElement) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  terrariumElement.onpointerdown = pointerDrag;
+
+  
+  function pointerDrag(e) {
+      e.preventDefault();
+      console.log(e);
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onpointermove = elementDrag;
+      document.onpointerup = stopElementDrag;
+    }
+    
+    function elementDrag(e) {
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        
+        terrariumElement.style.top = terrariumElement.offsetTop - pos2 + "px";
+        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + "px";
+    }
+    
+    function stopElementDrag() {
+        document.onpointerup = null;
+        document.onpointermove = null;
+  }
+}
+
+
+
+drageElement(document.getElementById("plant1"));
+drageElement(document.getElementById("plant2"));
+drageElement(document.getElementById("plant3"));
+drageElement(document.getElementById("plant4"));
+drageElement(document.getElementById("plant5"));
+drageElement(document.getElementById("plant6"));
+drageElement(document.getElementById("plant7"));
+drageElement(document.getElementById("plant8"));
+drageElement(document.getElementById("plant9"));
+drageElement(document.getElementById("plant10"));
+drageElement(document.getElementById("plant11"));
+drageElement(document.getElementById("plant12"));
+drageElement(document.getElementById("plant13"));
+drageElement(document.getElementById("plant14"));
